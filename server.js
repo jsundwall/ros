defc256aad279d8f486e698a1c519230e1e09337
Johan <johan.sundwall@gmail.com>
2016-12-17 03:02:34 +0100

    Turning off debugging

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.set('port', process.env.PORT || config.port);
 
 var server = app.listen(app.get('port'), function() {
   console.log("Running on port " + config.port)
-  debug('Express server listening on port ' + server.address().port);
+  // Next line is Node Debugger - Check docu on debugger if necessary  - only
+  // turn on the debugger if there are any issues.
+  
+  // debug('Express server listening on port ' + server.address().port);
 });
